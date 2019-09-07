b7fc2e1fa72f50d2c5b32e8c770e0e3a9fe35e7a
taimoor1122 <mtamoor981@gmail.com>
2019-09-07 23:31:30 +0500

    add get reqest and post request  and object by name orm

--- Controllers/firstController.cs
+++ Controllers/firstController.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using assessment1.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace assessment1.Controllers
 {
     public class firstController : Controller
     {
+        private DESIGNContext orm = null;
+        public firstController (DESIGNContext _orm)
+        {
+            orm = _orm;
+        }
         public IActionResult Index()
         {
             return View();
         }
+        [HttpGet]
+        public IActionResult homepage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult homepage(Category c)
+        {
+            orm.Category.Add(c);
+            orm.SaveChanges();
+            return View();
+        }
+
+
+
+
     }
 }
