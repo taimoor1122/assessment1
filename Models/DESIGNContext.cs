35af82d0621181adddaacf5e3f683c45b5de5dfd
taimoor1122 <mtamoor981@gmail.com>
2019-09-07 23:01:55 +0500

    comment line_num 21to28 in designcontext.cs

--- Models/DESIGNContext.cs
+++ Models/DESIGNContext.cs
@@ -18,14 +18,14 @@ namespace assessment1.Models
         public virtual DbSet<Category> Category { get; set; }
         public virtual DbSet<Post> Post { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-9K70R8O;Database=DESIGN;Trusted_Connection=True;User ID=sa; Password=morroo;");
-            }
-        }
+//        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("Server=DESKTOP-9K70R8O;Database=DESIGN;Trusted_Connection=True;User ID=sa; Password=morroo;");
+//            }
+//        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
